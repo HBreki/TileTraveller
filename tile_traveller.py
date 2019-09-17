a3f48db615f078d3d5575b027e9072b3d5114a81
Admin <brekivalsson@Brekis-MacBook-Pro.local>
2019-09-17 09:49:44 +0000

    second commit

---
+++ tile_traveller.py
@@ -0,0 +1,7 @@
+#Algorithm
+#x and y axis 
+#N and S are + and - on y-axis
+#E and W are + and - on x-axis
+#Constant immitations so you can't go through certain points
+#ask for player direction input
+#when x-axis is 3 and y-axis is 1. Program prints victory and stops running
