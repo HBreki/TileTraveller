3b0d6785c0533aa2ab20063eda43f04a6694ede8
Sævar Örn Sævarsson <sveppi23@gmail.com>
2019-09-17 10:20:02 +0000

    Breytingar

--- tile_traveller.py
+++ tile_traveller.py
@@ -4,4 +4,5 @@
 #E and W are + and - on x-axis
 #Constant immitations so you can't go through certain points
 #ask for player direction input
-#rueoiauorieau8r8eawhen x-axis is 3 and y-axis is 1. Program prints victory and stops running
+#when x-axis is 3 and y-axis is 1. Program prints victory and stops running
+#Faggot
