041a4bc38172fd62a208bd2d70fd81cdce73335a
Sævar Örn Sævarsson <sveppi23@gmail.com>
2019-09-17 10:48:37 +0000

    Change 3

--- tile_traveller.py
+++ tile_traveller.py
@@ -5,13 +5,3 @@
 #Constant immitations so you can't go through certain points
 #ask for player direction input
 #when x-axis is 3 and y-axis is 1. Program prints victory and stops running
-
-r = True
-x = 1
-y = 1
-direction = ""
-north = "(N)orth"
-south = "(S)outh"
-east = "(E)ast"
-west = "(W)est"
-o = "or"
